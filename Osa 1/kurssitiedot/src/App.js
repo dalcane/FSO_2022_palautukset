a2dbbbd197c8a8f938692e35641ddb37ef8e52f0
Antti H <akthurme@gmail.com>
2022-05-25 10:32:07 +0300

    Kurssitiedot commit.

--- Osa 1/kurssitiedot/src/App.js
+++ Osa 1/kurssitiedot/src/App.js
@@ -1,68 +1,81 @@
 const App = () => {
     const course = {
         name: 'Half Stack application development',
-        parts :[
+        parts: [
             {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
         ]
     }
-        return(
-    <div>
-        <Header course={course.name}/>
-        <Content parts={course.parts}/>
-        <Total/>
-    </div>
-)
+    return (
+        <div>
+            <Header course={course.name}/>
+            <Content parts={course.parts}/>
+            <Total parts={course.parts}/>
+        </div>
+    )
 
 }
 
 const Header = (props) => {
     console.log(props)
-    return(
-    <h1> {props.course}</h1>
+    return (
+        <h1> {props.course}</h1>
     )
 }
 
-//Sisältökomponentti
+//Sisältökomponentti, joka
 
 const Content = (props) => {
     console.log(props)
     const courses = props.parts;
     const statList = courses.map((kurssi, index) =>
-        <li key={"itam-${index}"}> {kurssi.name}, laajuus {kurssi.exercises} tehtävää</li>
+        <li key={"itam-${index}"}>
+            <Part name={kurssi.name} exercises={kurssi.exercises}/>
+        </li>
     );
-        return (
-            <div>
-                <ul>{statList}</ul>
-            </div>
-        );
-
-    }
-
-
-
-const Part = (props) => {
-    console.log(props)
-  return(
-      <div>
-          <p> {props.name} {props.exercises}</p>
-      </div>
-
-  )
+    return (
+        <div>
+            <ul>{statList}</ul>
+        </div>
+    );
 }
 
-const Total = (props) => {
+//osakomponentti
+const Part = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <p> Osan nimi on {props.name}
+                ja laajuus on {props.exercises} tehtävää</p>
+        </div>
 
+    )
+}
+
+//summakomponentti
+
+const Total = (props) => {
+    const courses = props.parts;
+    let sum = 0
+    const summa = courses.map((kurssi) => {
+            return sum += kurssi.exercises;
+        }
+    )
+    return (
+        <div>
+            Laajuus yhteensä on {sum} tehtävää.
+        </div>
+    );
 }
 
 export default App

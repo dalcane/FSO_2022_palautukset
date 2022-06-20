7b162fbcdbc2b7d5dd454d8c78f262d73c25917e
Antti H <akthurme@gmail.com>
2022-06-20 17:27:21 +0300

    Maiden tiedot 2.12 tehty.

--- Osa 2/maiden tiedot/src/App.js
+++ Osa 2/maiden tiedot/src/App.js
@@ -1,11 +1,42 @@
 import './App.css';
 import {useEffect, useState} from "react";
 import axios from "axios";
+import Notification from "./components/Notification";
 
+const ShowOne = ({countries1}) =>{
+    let maat= countries1.map(c => ({ name: c.name.common,
+        capital: c.capital, area: c.area, languages: c.languages, flag: c.flags.png }))
+    let languages = maat.map (c => (c.languages))
+    return (
+        <div>
+            <h2>{maat.map(capital=>capital.name)}</h2>
+            <div></div>
+            Capital: {maat.map(capital=>capital.capital)}
+            <div></div>
+            Area: {maat.map(c=>c.area)}
+            <div></div>
+            Languages:
+            {Object.entries(languages[0]).map(
+                ([key,value])=> {
+                    return <li> {key} - {value}</li>;
+                }
+            )}
+            <div></div>
+            Flag:
+            <div></div>
+            <img src= {maat.map(c=>c.flag)} alt="Flag"/>
+        </div>
+    )
+}
+
+function boom() {
+    window.alert("Can't display this, sry.")
+    }
 
 const ShowCountries = ({countries}) =>{
     let maat= countries.map(c => ({ name: c.name.common,
-        capital: c.capital, area: c.area, languages: c.languages, flag: c.flag }))
+        capital: c.capital, area: c.area, languages: c.languages, flag: c.flags.png }))
+    let languages = maat.map (c => (c.languages))
     if (maat.length===0){
         return (
             <div>make a search</div>
@@ -19,13 +50,13 @@ const ShowCountries = ({countries}) =>{
     if (maat.length<10 && maat.length>1) {
         return (
             <div>
-                {maat.map(name=> <li>{name.name}</li>)}
+                {maat.map(name=> <li>{name.name}
+                    <button value={name.name} onClick={boom} name={name.name}>Show
+                    </button></li>)}
             </div>
         )
     }
     if (maat.length === 1) {
-        console.log("countries",countries)
-        console.log("maat",maat)
         return (
             <div>
             <h2>{maat.map(capital=>capital.name)}</h2>
@@ -35,11 +66,15 @@ const ShowCountries = ({countries}) =>{
                 Area: {maat.map(c=>c.area)}
                 <div></div>
                 Languages:
-                {maat.map(c=> <li>{(c.languages.data)}</li>)}
+                {Object.entries(languages[0]).map(
+                    ([key,value])=> {
+                        return <li> {key} - {value}</li>;
+                    }
+                )}
                 <div></div>
                 Flag:
                 <div></div>
-                {maat.map(c=>c.flag)}
+                <img src= {maat.map(c=>c.flag)} alt="Flag"/>
             </div>
         )
     }
@@ -49,6 +84,7 @@ function App() {
 
     const [countries, setCountries] = useState([])
     const [searchInput, setSearchInput] = useState('')
+    const [countryStats, setCountryStats] = useState(null)
 
     const handleSearch = (event) =>{
         event.preventDefault();
@@ -68,16 +104,15 @@ function App() {
     })
 
 
-
-
   return (
     <div>
+        <Notification message={countryStats} />
       Find countries: <input
         type="search"
         value={searchInput}
         onChange={handleSearch}/>
         <div>
-            <ShowCountries countries={countries}/>
+            <ShowCountries countries={countries} countries1={countries}/>
         </div>
     </div>
 
